1569d316c432bf5b2e8724aa19e5c20d80f952a7
Yuliia Porkhun <julposv@gmail.com>
2023-04-21 13:04:09 +0300

    adding lesson 03

---
+++ IsoscelesTriangle.java
@@ -0,0 +1,21 @@
+
+package ua.hillel.lesson03;
+import java.util.Scanner;
+
+public class IsoscelesTriangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введіть сторону 1: ");
+        int side1 = scanner.nextInt();
+        System.out.print("Введіть сторону 2: ");
+        int side2 = scanner.nextInt();
+        System.out.print("Введіть сторону 3: ");
+        int side3 = scanner.nextInt();
+
+        if ((side1 == side2) && side3 != side1 || (side2== side3) && side1 != side2|| (side3 == side1) && side2 != side1) {
+            System.out.println("Трикутник є рівнобедреним");
+        } else {
+            System.out.println("Трикутник не рівнобедрений");
+        }
+    }
+}
